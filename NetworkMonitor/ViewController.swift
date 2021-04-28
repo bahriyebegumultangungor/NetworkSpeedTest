1e2a0f999cd6bdc19d75297377ca641c7d7c8df0
Begüm <begumultan@gmail.com>
2021-04-28 15:59:52 +0300

    Initial Commit

---
+++ NetworkMonitor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NetworkMonitor
+//
+//  Created by Bahriye Begüm Ültan Güngör on 28.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
