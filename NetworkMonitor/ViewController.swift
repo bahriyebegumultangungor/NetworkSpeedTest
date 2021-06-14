6715785bbce7020a9a88295938a231cabeb710d2
Begüm <begumultan@gmail.com>
2021-06-14 12:31:24 +0300

    first commit

--- NetworkMonitor/ViewController.swift
+++ NetworkMonitor/ViewController.swift
@@ -7,13 +7,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+class ViewController: UIViewController, NetworkSpeedProviderDelegate {
+    func callWhileSpeedChange(networkStatus: NetworkStatus) {
+        switch networkStatus {
+        case .poor:
+            break
+        case .good:
+            break
+        case .disConnected:
+            break
+        }
     }
+    
+    let test = NetworkSpeedTest()
+    
+    
+    override func viewDidLoad() {
+           super.viewDidLoad()
+           test.delegate = self
+           test.networkSpeedTestStop()
+           test.networkSpeedTestStart(UrlForTestSpeed: "https://giantbomb1.cbsistatic.com/uploads/original/15/157771/2312725-a10.jpeg")
+           // Do any additional setup after loading the view.
+       }
+   }
 
-
-}
-
+////        test.networkSpeedTestStart(UrlForTestSpeed:"https://images.apple.com/v/imac-with-retina/a/images/overview/5k_image.jpg")
+//        test.networkSpeedTestStart(UrlForTestSpeed:"https://giantbomb1.cbsistatic.com/uploads/original/15/157771/2312725-a10.jpeg")
+////        test.networkSpeedTestStart(UrlForTestSpeed:"https://i.ibb.co/FxQqWsH/unnamed.jpg")
